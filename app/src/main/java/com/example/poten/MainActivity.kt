868fb05c17d054a31981feaeff33f4daa3af795b
jeongminhui99 <jeongminhee99@naver.com>
2022-09-13 01:05:16 +0900

    Feat: Connect Intellij

--- app/src/main/java/com/example/poten/MainActivity.kt
+++ app/src/main/java/com/example/poten/MainActivity.kt
@@ -2,18 +2,51 @@ package com.example.poten
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.poten.Home.HomeActivity
+import com.example.poten.dto.Test
+import com.example.poten.interfaces.RetrofitService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var button : Button
+    lateinit var restText : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var retrofit = Retrofit.Builder().baseUrl("http://192.168.35.193:8080/").addConverterFactory(GsonConverterFactory.create()).build();
+        val service = retrofit.create(RetrofitService::class.java);
+
+
         button = findViewById(R.id.button)
+        restText = findViewById(R.id.text_rest_api)
+
+        service.getTest().enqueue(object: Callback<Test>{
+            override fun onResponse(call: Call<Test>, response: Response<Test>) {
+                if(response.isSuccessful){
+                    var result: Test? = response.body();
+                    println("success" + result.toString());
+                    restText.setText(result.toString());
+                }else{
+                    println("여기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ 실패");
+                }
+            }
+
+            override fun onFailure(call: Call<Test>, t: Throwable) {
+                println("여기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ 실패222");
+                Log.d("YMC", "onFailure 에러: " + t.message.toString());
+            }
+        })
+
 
         button.setOnClickListener(View.OnClickListener {
             val intent1 = Intent(this, HomeActivity::class.java) //ACTIVITY_NUM = 0
---
+++ app/src/main/java/com/example/poten/interfaces/RetrofitService.kt
@@ -0,0 +1,11 @@
+package com.example.poten.interfaces
+
+import com.example.poten.dto.Test
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RetrofitService {
+    @GET("clubs/test")
+    //fun getTest(): String
+    fun getTest(): Call<Test>
+}
