cdcc82eb1033947bd021594fb290aba989c28aaf
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2022-09-13 11:55:07 +0900

    Feat: Connect Feed and Fix Layout

--- app/src/main/java/com/example/poten/Home/HomeActivity.kt
+++ app/src/main/java/com/example/poten/Home/HomeActivity.kt
@@ -6,6 +6,11 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.poten.R
 import com.example.poten.Utils.BottomNavigationViewHelper
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -20,6 +25,8 @@ class HomeActivity : AppCompatActivity() {
     private var mFrameLayout: FrameLayout? = null
     private var mRelativeLayout: RelativeLayout? = null
 
+    private lateinit var adapter: PostListViewAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +35,34 @@ class HomeActivity : AppCompatActivity() {
         mFrameLayout = findViewById<View>(R.id.container) as FrameLayout
         mRelativeLayout = findViewById<View>(R.id.relLayoutParent) as RelativeLayout
 
+
+        var rv_postList: RecyclerView = findViewById<RecyclerView>(R.id.home_recyView) as RecyclerView
+        // 리사이클러뷰 어댑터에 화면에 띄울 데이터를 넘긴다.
+        adapter = PostListViewAdapter(getApplicationContext());
+
+        // 어댑터 연결
+        var manager = LinearLayoutManager (getApplicationContext())
+        manager.reverseLayout = true
+        manager.stackFromEnd = true
+
+        rv_postList.layoutManager = manager
+        rv_postList.setHasFixedSize(true)
+        rv_postList.adapter = adapter
+
+        // 하드코딩
+        var postList =  mutableListOf<Post>()
+        postList.add(Post("cookingTeens", "test1", "1분전", 1L, 2L, 1, 5, "contentsssssss"))
+
+        // 데이터 연결
+        adapter.setListData(postList)
+
+
+        // 네비게이션 뷰
         setupBottomNavigationView()
     }
+
+
+
 
     private fun setupBottomNavigationView() {
         //Log.d(HomeActivity.TAG, "setupBottomNavigationView: setting up BottomNavigationView")
@@ -53,4 +86,5 @@ class HomeActivity : AppCompatActivity() {
             showLayout()
         }
     }
+
 }
