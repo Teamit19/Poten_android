7c4228a907574bd04c07df5e979d463af895208e
jeongminhui99 <jeongminhee99@naver.com>
2022-09-13 23:32:08 +0900

    Feat: Connect Intellij with Android(Pics)

--- app/src/main/java/com/example/poten/interfaces/RetrofitService.kt
+++ app/src/main/java/com/example/poten/interfaces/RetrofitService.kt
@@ -1,10 +1,26 @@
 package com.example.poten.interfaces
 
 import com.example.poten.dto.Test
+import com.example.poten.dto.Test2
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface RetrofitService {
-    @GET("clubs/test")
-    fun getTest(): Call<Test>
+//    @GET("clubs/test")
+//    fun getTest(): Call<Test>
+    //@Multipart
+    @POST("clubs/test")
+    fun sendTest(@Body data: HashMap<String, String>): Call<Test>
+    //fun sendTest(@PartMap data: HashMap<String, String>): Call<Test>
+
+    @POST("clubs/test2")
+    @Multipart
+    fun picTest(
+        //@Part pics: List<MultipartBody.Part>,
+        @Part pics: MultipartBody.Part,
+        @Part("content") content: String,
+        @Part("content2") content2: String
+    ): Call<Test2>
 }
